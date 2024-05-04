28ae1d9028e957709e8114ba57a4313c284a6c58
Rómulo <romulo3218@gmail.com>
2024-05-04 12:16:49 +0100

    add compute lexical similarity and a test notebook

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
 from owlready2 import *
 import os
+import re
+import Levenshtein
 
 def loadOntology(path: str) -> Ontology:
-    print(path)
+    # print(path)
     if os.path.exists(path):
         return get_ontology(path).load()
     else:
@@ -14,3 +16,29 @@ def get_labels(ontology):
     raw_labels = list(map(lambda entity: entity.label[0] if len(entity.label) > 0 else "", ontology.classes()))
     filtered_labels = list(filter(lambda label: label != "", raw_labels))
     return filtered_labels
+
+def normalize_labels(label: list, method) -> list:
+    if method == "Jaccard":
+        return list(map(lambda l: set(normalize_string(l).split()), label))
+    elif method == "Levenshtein":
+        return list(map(lambda l: normalize_string(l), label))
+    elif method == "Cosine":
+        # Not implmented yet
+        return []
+
+def normalize_string(s):
+    # Convert to lowercase
+    s = s.lower()
+    # Remove non-alphanumeric characters and spaces
+    s = re.sub(r'[^a-zA-Z0-9\s]', ' ', s)
+    # Remove extra spaces
+    s = re.sub(r'\s+', ' ', s).strip()
+    return s
+
+def jaccard_similarity(set1, set2):
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    return intersection / union if union != 0 else 0.0
+
+def levenshtein_similarity(labels1, labels2):
+    return Levenshtein.distance(labels1, labels2)
--- ontolgyAlignment.py
+++ ontolgyAlignment.py
@@ -1,11 +1,10 @@
 from functions import *
-
-
+import time
 
 # I think we should have a class for the ontology alignment, where executes all the pipeline.
 # With this class we can personalize the pipeline, running different ways to align ontologies.
 
-class OntologyAlignmet:
+class OntologyAlignment:
 
     def __init__(self, path_o1, path_o2, lexical_similarity= "Jaccard", LLM=True) -> None:
         
@@ -14,6 +13,8 @@ class OntologyAlignmet:
 
         self.labels_o1 = get_labels(self.ontology1)
         self.labels_o2 = get_labels(self.ontology2)
+
+        self.lexical_similarity = lexical_similarity
 
     # Ontology is 1 or 2 
     def getClassesOntology(self, ontology:int) -> list:
@@ -39,6 +40,26 @@ class OntologyAlignmet:
             return self.ontology2
         else:
             raise ValueError("The ontology must be 1 or 2")
+        
+    def compute_lexical_similarity(self) -> dict:
+        labels1, labels2 = normalize_labels(self.labels_o1, self.lexical_similarity), normalize_labels(self.labels_o2, self.lexical_similarity)
+        func = jaccard_similarity 
+        if self.lexical_similarity == "Levenshtein":
+            func = levenshtein_similarity
+        elif self.lexical_similarity == "Cosine":
+            pass
+
+        # print(labels1)
+
+        scores = {}
+        for l1 in labels1:
+            for l2 in labels2:
+                str_l1 = " ".join(l1) if self.lexical_similarity == "Jaccard" else l1
+                str_l2 = " ".join(l2) if self.lexical_similarity == "Jaccard" else l2
+                scores[(str_l1, str_l2)] = func(l1, l2)
+
+        return scores
+
 
 
     def startAlignment(self):
@@ -50,3 +71,10 @@ class OntologyAlignmet:
 # ONTOLOGIES = os.getcwd() + "\\ontologies\\anatomy\\"
 # oa = OntologyAlignmet(ONTOLOGIES + "human.owl", ONTOLOGIES + "mouse.owl")
 # print(oa.getLabelsOntology(1))
+# print(oa.getLabelsOntology(2))
+
+
+
+# t1 = time.time()
+# print(oa.compute_lexical_similarity("Jaccard"))
+# print("Time: ", time.time() - t1)
